6181a7d3cb45dcaed9914d00a611917b21cf2139
yglee <23801504@qq.com>
2019-02-20 22:10:59 +0800

    添加逻辑

--- pages/addticket/addticket.js
+++ pages/addticket/addticket.js
@@ -20,10 +20,10 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    // wx.showLoading({
-    //   title: '加载页面数据...',
-    //   mask: true
-    // })
+    wx.showLoading({
+      title: '加载页面数据...',
+      mask: true
+    })
     var that = this,
       tickettotalcountUrl = api.tickettotalcountUrl,
       ticketvalidcountUrl = api.ticketvalidcountUrl,
