c3593e3198dd102980723de7a616d5e54849bf71
yglee <23801504@qq.com>
2019-03-21 16:25:18 +0800

    优化添加卡券算法

--- pages/addticket/addticket.js
+++ pages/addticket/addticket.js
@@ -24,88 +24,6 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    //  canvas画券封面
-    // function drawCover(expectticket) {
-    //   let that = this,
-    //     ctx = wx.createCanvasContext('coverCanv');
-    //   ctx.drawImage('../../assets/ticketpattern.png', 0, 0, 496, 189);
-    //   ctx.setFontSize(22);
-    //   ctx.setFillStyle('white');
-    //   ctx.fillText(expectticket.productname, 105, 108);
-    //   ctx.scale(2, 2);
-    //   ctx.draw(false, function () {
-    //     wx.canvasToTempFilePath({
-    //       canvasId: 'coverCanv',
-    //       success: (res) => {
-    //         uploadCover(res.tempFilePath, expectticket);
-    //       }
-    //     })
-    //   })
-    // }
-
-    //  上传图片
-    // function uploadCover(cover, expectticket) {
-    //   let uploadUrl = api.uploadUrl;
-    //   wx.uploadFile({
-    //     url: uploadUrl,
-    //     filePath: cover,
-    //     name: 'cover',
-    //     success: (res) => {
-    //       wx.hideLoading();
-    //       // wx.showToast({
-    //       //   title: '上传成功！',
-    //       //   image: '../../assets/success.png',
-    //       //   duration: 2000
-    //       // })
-    //       var coverUrl = JSON.parse(res.data).coverUrl;
-    //       that.setData({
-    //         cover: coverUrl
-    //       })
-
-    //       updateCover(expectticket);
-    //     },
-    //     fail: (err) => {
-    //       wx.hideLoading();
-    //       // wx.showToast({
-    //       //   title: '上传失败~~',
-    //       //   image: '../../assets/fail.png',
-    //       //   duration: 2000
-    //       // })
-    //     }
-    //   })
-    // }
-
-    //  更新申领图片封面
-    // function updateCover(expectticket) {
-    //   let updateexpectticketcoverUrl = api.updateexpectticketcoverUrl,
-    //     sqlParam = 'cover',
-    //     sqlValue = that.data.cover,
-    //     rangeParam = 'e_id',
-    //     rangeValue = expectticket.e_id;
-    //   wx.request({
-    //     url: updateexpectticketcoverUrl,
-    //     method: 'POST',
-    //     data: {
-    //       sqlParam: sqlParam,
-    //       sqlValue: sqlValue,
-    //       rangeParam: rangeParam,
-    //       rangeValue: rangeValue
-    //     },
-    //     success: (res) => {
-    //       // wx.showToast({
-    //       //   title: '封面更新成功',
-    //       //   icon: 'none'
-    //       // })
-    //     },
-    //     fail: (err) => {
-    //       wx.showToast({
-    //         title: '网络错误',
-    //         icon: 'none'
-    //       })
-    //     }
-    //   })
-    // }
-
     //  请求卡券总数 查出最后一条申请卡券的记录
     var expectAuthRequest = new Promise(function (resolve, reject) {
       var that = this,
@@ -197,9 +115,6 @@ Page({
           duration: 2000
         })
       })
-
-      // if (res.cover != null) return;
-      // drawCover(res);
     })
   },
 
@@ -259,12 +174,79 @@ Page({
   },
 
   /**
+   * 生成券码请求  'productid': 1014519
+   */
+  ticketgenRequest: function (success, fail) {
+    var now = dateUtil.formatTime(new Date());
+    var regInfo = wx.getStorageSync('regInfo');
+    var wxopenid = wx.getStorageSync('wxopenid');
+    var content = {
+      'productid': this.data.netbakeid,
+      'customername': '皇冠蛋糕',
+      'datasource': 11,
+      'timestamp': now,
+      'wxopenid': wxopenid
+    };
+    var ticketGenUrl = api.ticketGenUrl;
+    var encContent = urlSafeBase64.encode(api.encryptContent(content));
+    var sign = api.sign(content);
+    var token = api.token;
+
+    wx.request({
+      url: ticketGenUrl,
+      method: 'POST',
+      data: {
+        token: token,
+        sign: sign,
+        content: encContent
+      },
+      success: function (res) {
+        var data = JSON.parse(res.data);
+        var ticket = api.decryptContent(data.content);
+        success(ticket);
+      },
+      fail: function (err) {
+        fail(err);
+      }
+    })
+  },
+
+  /**
+   * 券卡插入数据库请求
+   */
+  addToDBRequest: function (ticket, success, fail) {
+    var addticketUrl = api.addticketUrl;
+    var wxopenid = wx.getStorageSync('wxopenid');
+    var company = wx.getStorageSync('regInfo').company;
+    wx.request({
+      url: addticketUrl,
+      method: 'POST',
+      data: {
+        wxopenid: wxopenid,
+        company: company,
+        ticketcode: ticket.ticketcode,
+        ticketno: ticket.ticketno,
+        productname: ticket.productname,
+        price: ticket.price,
+        distributestatus: 0, // 状态0为未分发 1为已分发
+        distributedate: dateUtil.formatTime(new Date()),
+        expectdate: this.data.expectdate
+      },
+      success: function (res) {
+        success(res);
+      },
+      fail: function (err) {
+        fail(err);
+      }
+    })
+  },
+
+  /**
    * 统一处理添加卡券
    */
   addtickets: function () {
     // 客户无法超越申请数量的券总数
-    var that = this,
-      totalcount = this.data.totalcount,
+    var totalcount = this.data.totalcount,
       addcount = Number(this.data.addcount) + Number(this.data.numbers);
     if (addcount > totalcount) {
       wx.showToast({
@@ -276,6 +258,7 @@ Page({
       return;
     }
 
+    //  日期比较
     function compareDate(date1, date2) {
       var result = false;
       if (date1.getFullYear() > date2.getFullYear()) {
@@ -305,106 +288,38 @@ Page({
       return;
     }
 
-    //  生成券码请求  'productid': 1014519,
-    var ticketgenRequest = new Promise(function (resolve, reject) {
-      var now = dateUtil.formatTime(new Date());
-      var regInfo = wx.getStorageSync('regInfo');
-      var wxopenid = wx.getStorageSync('wxopenid');
-      var content = {
-        'productid': that.data.netbakeid,
-        'customername': '皇冠蛋糕',
-        'datasource': 11,
-        'timestamp': now,
-        'wxopenid': wxopenid
-      };
-      var ticketGenUrl = api.ticketGenUrl;
-      var encContent = urlSafeBase64.encode(api.encryptContent(content));
-      var sign = api.sign(content);
-      var token = api.token;
 
-      wx.request({
-        url: ticketGenUrl,
-        method: 'POST',
-        data: {
-          token: token,
-          sign: sign,
-          content: encContent
-        },
-        success: function (res) {
-          resolve(res);
-        },
-        fail: function (err) { }
-      })
-    });
-
-    // 券卡插入数据库请求
-    var addToDBRequest = function (ticket, success, fail) {
-      var addticketUrl = api.addticketUrl;
-      var wxopenid = wx.getStorageSync('wxopenid');
-      var company = wx.getStorageSync('regInfo').company;
-      wx.request({
-        url: addticketUrl,
-        method: 'POST',
-        data: {
-          wxopenid: wxopenid,
-          company: company,
-          ticketcode: ticket.ticketcode,
-          ticketno: ticket.ticketno,
-          productname: ticket.productname,
-          price: ticket.price,
-          distributestatus: 0, // 状态0为未分发 1为已分发
-          distributedate: dateUtil.formatTime(new Date()),
-          expectdate: that.data.expectdate
-        },
-        success: function (res) {
-          success(res);
-        },
-        fail: function (err) {
-          fail(err);
-        }
-      })
-    };
-
-    var that = this;
-    var numbers = that.data.numbers;
-    var addcount = that.data.addcount;
+    var addcount = this.data.addcount,
+      numbers = this.data.numbers;
     wx.showLoading({
       title: '添加中...',
+      mask: true
     })
 
-    return new Promise(function (resolve, reject) {
-      ticketgenRequest.then(function (res) {
-        var data = JSON.parse(res.data);
-        var ticket = api.decryptContent(data.content);
-        if (data.code == 0 && numbers > 0) {
-          resolve();
-          numbers--;
-          that.setData({
-            numbers: numbers,
-            addBtnStatus: false
-          })
-          addToDBRequest(ticket, function (res) {
-            addcount++;
-            that.setData({
-              addcount: addcount
-            })
-            wx.setStorageSync('addcount', addcount);
-            that.addtickets();
-          }, function (err) {
-            reject(err);
-          })
-        } else {
-          wx.hideLoading();
-        }
-      })
-    }).then(function () {
-      if (that.data.numbers == 0) {
-        that.setData({
-          numbers: ''
-        })
-      }
+    numbers--;
+    this.setData({
+      numbers: numbers
+    })
+
+    if (numbers <= -1) {
       wx.hideLoading();
-    }).catch(function (err) {
+      this.setData({
+        numbers: 0,
+        addBtnStatus: false
+      })
+      return;
+    }
+
+    this.ticketgenRequest((ticket) => {
+      this.addToDBRequest(ticket, (res) => {
+        addcount++;
+        this.setData({
+          addcount: addcount
+        })
+        wx.setStorageSync('addcount', addcount);
+        this.addtickets();
+      })
+    }, (err) => {
       wx.showToast({
         title: '添加失败！',
         image: '../../assets/fail.png',
